c1912794449e5222293a47bfd701a65f83316fc2
César Venzor <grayfox15@protonmail.ch>
2017-10-28 19:25:47 -0600

    el slider mas feo del mundo

--- www/js/index.js
+++ www/js/index.js
@@ -4,11 +4,49 @@ var traducir = function () {
     
     s = s.toUpperCase();
 
+    document.getElementById('board').innerHTML = "\
+    	<div id='carrusel' class='carousel slide' data-ride='carousel'>\
+	    <ol id='lista' class='carousel-indicators'>\
+	    </ol>\
+	    <div id='items' class='carousel-inner' role='listbox'>\
+	    </div>\
+	    <a class='carousel-control-prev' href='#carrusel' role='button' data-slide='prev'>\
+		<span class='carousel-control-prev-icon' aria-hidden='true'></span>\
+		<span class='sr-only'>Previous</span>\
+	    </a>\
+	    <a class='carousel-control-next' href='#carrusel' role='button' data-slide='next'>\
+		<span class='carousel-control-next-icon' aria-hidden='true'></span>\
+		<span class='sr-only'>Next</span>\
+	    </a>\
+	</div> ";
+
+    var contador = 0;
+        
     for (var i = 0; i < s.length; i++) {
 	archivo = s[i] + ".png";
-	
-	alert(archivo);
-    }
 
+	//var li = document.createElement('li');
+	
+	//li = "<li data-target='#carrusel' data-slide-to='" + contador + "'></li>";
+
+	var div = document.createElement('div');
+	if (contador == 0){
+	    div.className = "carousel-item active";
+	} else {
+	    div.className = 'carousel-item';
+	}
+	
+	div.innerHTML = "<img class='d-block img-fluid' src='../img/" + archivo + "' />";
+
+	console.log("ok " + archivo);
+
+	document.getElementById('items').appendChild(div);
+	contador++;
+    }
     document.getElementById('intext').value = '';
+
+    /*
+    var board = document.getElementById('board').innerHTML;
+    document.getElementById('board').innerHTML = board;
+    */
 }
